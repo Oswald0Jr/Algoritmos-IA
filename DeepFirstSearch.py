5daf6b97efefe96cee8bf79bdf35a4e47b73dc68
Oswaldo Jr <oswaldojose590@gmail.com>
2023-06-28 17:02:52 -0300

    Update DeepFirstSearch.py

--- DeepFirstSearch.py
+++ DeepFirstSearch.py
@@ -1,1 +1,12 @@
+def busca_em_profundidade(grafo, inicio, objetivo):
+    # Inicializa a pilha de busca
+    pilha = [inicio]
+
+    # Inicializa o dicionário de pais
+    pais = {inicio: None}
+
+    # Enquanto houver nós a serem visitados
+    while pilha:
+        # Retira o último nó da pilha
+        atual = pilha.pop()
 
