d0fd00851ba74e5fc5b85895ec4d3704aba288ba
Oswaldo Jr <oswaldojose590@gmail.com>
2023-07-02 17:42:56 -0300

    Update DeepFirstSearch.py

--- DeepFirstSearch.py
+++ DeepFirstSearch.py
@@ -10,3 +10,36 @@ def busca_em_profundidade(grafo, inicio, objetivo):
         # Retira o último nó da pilha
         atual = pilha.pop()
 
+        # Se encontramos o objetivo, retorna o caminho percorrido
+        if atual == objetivo:
+            caminho = []
+            while atual != None:
+                caminho.append(atual)
+                atual = pais[atual]
+            return caminho[::-1]
+
+        # Para cada vizinho do nó atual
+        for vizinho in grafo[atual]:
+            # Se ainda não visitamos o vizinho
+            if vizinho not in pais:
+                # Adiciona o vizinho à pilha
+                pilha.append(vizinho)
+
+                # Define o pai do vizinho como sendo o nó atual
+                pais[vizinho] = atual
+
+    # Se chegamos aqui, não há caminho possível
+    return None
+
+# Pede ao usuário a cidade de origem e de destino
+origem = input("Digite a cidade de origem: ")
+destino = input("Digite a cidade de destino: ")
+
+# Encontra o caminho entre as cidades
+caminho = busca_em_profundidade(grafo, origem, destino)
+
+# Imprime o resultado
+if caminho is None:
+    print("Caminho não encontrado!")
+else:
+    print("Caminho: ", caminho)
