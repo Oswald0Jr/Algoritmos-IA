86346a3677ff7bcc0af2d36cc293c795883a083b
Oswaldo Jr <oswaldojose590@gmail.com>
2023-07-31 14:24:25 -0300

    Update and rename Hanoi_Tower_5_Rings to Hanoi_Tower_5_Rings.py

---
+++ Hanoi_Tower_5_Rings.py
@@ -0,0 +1,18 @@
+def hanoi(n, origem, destino, auxiliar):
+    if n == 1:
+        print(f"Mova o disco 1 de {origem} para {destino}")
+        return
+    hanoi(n-1, origem, auxiliar, destino)
+    print(f"Mova o disco {n} de {origem} para {destino}")
+    hanoi(n-1, auxiliar, destino, origem)
+
+def espaco_de_estados_torre_hanoi(n):
+    print(f"Espaço de Estados para a Torre de Hanoi com {n} argolas:")
+    hanoi(n, 'A', 'C', 'B')
+
+# Teste com até 5 argolas
+espaco_de_estados_torre_hanoi(1)
+espaco_de_estados_torre_hanoi(2)
+espaco_de_estados_torre_hanoi(3)
+espaco_de_estados_torre_hanoi(4)
+espaco_de_estados_torre_hanoi(5)
