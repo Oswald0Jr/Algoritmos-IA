792be3d70591ddc7c118e4178a5d9543fdd1f029
Oswaldo Jr <oswaldojose590@gmail.com>
2023-08-02 16:12:06 -0300

    Update Hanoi_Tower_5_Rings.py

--- Hanoi_Tower_5_Rings.py
+++ Hanoi_Tower_5_Rings.py
@@ -5,23 +5,28 @@ class EstadoHanoi:
     def __init__(self, torres, movimentos=None):
         self.torres = torres
         self.movimentos = movimentos if movimentos is not None else []
-
+        
+# Retorna uma representação em string do objeto
     def __str__(self):
         return str(self.torres)
-
+        
+# Cria uma cópia do estado atual da torre e retorna um novo objeto
     def copiar(self):
         return EstadoHanoi([torre[:] for torre in self.torres], self.movimentos[:])
-
+        
+# Move um disco da torre de origem para a torre de destino e atualiza o estado das torres e a lista de movimentos
     def mover_disco(self, origem, destino):
         novo_estado = self.copiar()
         disco = novo_estado.torres[origem].pop()
         novo_estado.torres[destino].append(disco)
         novo_estado.movimentos.append((origem, destino))
         return novo_estado
-
+        
+# Verifica o estado atual é o objetivo 
     def eh_estado_objetivo(self):
         return len(self.torres[0]) == 0 and len(self.torres[1]) == 0
-
+        
+# Obtem os movimentos possivel em torres não vazias e vê se o disco é menor
 def obter_movimentos_possiveis(estado_atual):
     movimentos_possiveis = []
     for origem in range(3):
@@ -30,7 +35,8 @@ def obter_movimentos_possiveis(estado_atual):
                 if origem != destino and (not estado_atual.torres[destino] or estado_atual.torres[origem][-1] < estado_atual.torres[destino][-1]):
                     movimentos_possiveis.append((origem, destino))
     return movimentos_possiveis
-
+    
+# Implementa o algoritmo (BFS)
 def bfs(torres_iniciais):
     fila = deque([EstadoHanoi(torres_iniciais)])
     visitados = set([str(torres_iniciais)])
